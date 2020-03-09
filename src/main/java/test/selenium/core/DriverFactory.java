6669bee1956355bd5394bdab28b2a501493233b9
Rafael Correia <rafaelcbm@gmail.com>
2020-03-09 13:57:09 -0300

    Using default chromedriver

--- src/main/java/test/selenium/core/DriverFactory.java
+++ src/main/java/test/selenium/core/DriverFactory.java
@@ -3,7 +3,6 @@ package test.selenium.core;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -20,11 +19,17 @@ public class DriverFactory {
 				driver = new FirefoxDriver();
 				break;
 			case CHROME:
-				System.setProperty("webdriver.chrome.driver",
-						"/home/users/rafael.bmenezes/dev/bin/chromedriver_linux64/chromedriver");
-				ChromeOptions options = new ChromeOptions();
-				options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
-				driver = new ChromeDriver(options);
+				/*
+				 * Additional path config for chrome driver.
+				 * 
+				 * System.setProperty("webdriver.chrome.driver",
+				 * "/home/users/rafael.bmenezes/dev/bin/chromedriver_linux64/chromedriver");
+				 * ChromeOptions options = new ChromeOptions();
+				 * options.setExperimentalOption("excludeSwitches", new String[] {
+				 * "enable-automation" }); driver = new ChromeDriver(options);
+				 */
+				
+				driver = new ChromeDriver();
 				break;
 			}
 			driver.manage().window().setSize(new Dimension(1200, 765));
